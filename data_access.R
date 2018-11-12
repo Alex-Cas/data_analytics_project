ed1c25950c2002633bcb60b99f8009beb991c273
AmirAL13 <amirkarimallal@gmail.com>
2018-11-12 01:46:48 +0100

    Add files via upload
    Modifs + Sans graphs Ratios

--- data_access.R
+++ data_access.R
@@ -6,17 +6,16 @@ library(tidyverse)
 library(dplyr)
 library(scales)
 library(lubridate)
+library(readxl)
 
 
 ########################
 #### INITIALIZATION ####
-#logs <- read_delim("../data/logs.csv", 
-#                   ";", escape_double = FALSE, locale = locale(encoding = "ASCII"), 
-#                   trim_ws = TRUE)
+logs <- read_delim("../data/logs.csv", 
+                   ";", escape_double = FALSE, locale = locale(encoding = "ASCII"), 
+                   trim_ws = TRUE)
 
-logs <- logs
-survey <- surveydataece
-
+survey <- read_excel("../data/surveydataece.xlsx" )
 
 # Special characters
 logs$User <- gsub("\u000e" , "\351" ,logs$User)
@@ -180,6 +179,161 @@ d.getDayUsageRatio_All <- function() {
   return (bar)
 }
 
+##### DATA ACCESS  #####
+########################
+
+
+
+
+# workspace fourre tout
+
+
+
+#data <- h_toDay(data)
+
+
+#data <- logs[logs$User == "Renaud Courbis",]
+
+
+#data["Time"] <- lapply(data["Time"], function(x) {dmy_hm(x, tz=Sys.timezone())})
+#data["Time"] <- lapply(data["Time"], function(x) {format(x, format="%Y-%m-%d")})
+#data["Time"] <- lapply(data["Time"], function(x) {as.numeric(as.POSIXct(x, format="%Y-%m-%d"))})
+#data <- rename(count(data, data$Type, data$Time), Type = "data$Type", Time = "data$Time")
+#data
+
+
+
+#g <- ggplot(data, aes(Time, n, color=Type))
+#g
+
+#bar <- g + geom_line(size=1) + stat_summary(fun.y = "sum",aes(Time,n,col = "sum"), size = 1, geom = "line") 
+#bar
+
+#bar <- g + geom_bar(stat="identity", width = 0.5) +
+#  labs(title="Total hourly usage") +
+#  theme(axis.text.x = element_text(angle=65, vjust=0.6))
+
+#bar
+
+
+
+
+#moyenne age
+#nombre de cigarette par user ? 
+
+#library(ggmap)
+#install.packages("leaflet", dependencies = TRUE)
+
+library(leaflet)
+
+#lyon=c(lon=4.842223 ,lat=45.759723 )
+#lyon_map=get_map(location=lyon)
+#ggmap(lyon_map)
+
+#ggmap(lyon_map_3)+
+#  geom_point(data=data_sp, 
+#             aes(x=lon,y=lat), col="red", size=2)
+
+# Définition d'un nouvel icône
+mapUser <- function(user,typeList){
+#Smoking <- makeIcon(
+  #iconUrl = "smoking.png",
+  #iconWidth = 40, iconHeight = 40)
+  data <- logs[logs$User == user,]
+  data <- data[data$Type %in% typeList,]
+  m <- leaflet() %>%
+  addTiles() %>%
+ # setView(lng = 2.292551, lat = 48.858255, zoom = 16) %>%
+  # addMarkers(lng = data$Longitude, lat = data$Latitude , color="red")
+  addCircles(lng = data$Longitude, lat = data$Latitude , color="red",fill = TRUE, opacity = 0.3 , weight=12 ,fillColor = "red",
+             fillOpacity = 0.2)
+  
+return(m)
+}
+
+mapAll <- function(typeList){
+  #Smoking <- makeIcon(
+  #iconUrl = "smoking.png",
+  #iconWidth = 40, iconHeight = 40)
+  data <- logs
+  data <- data[data$Type %in% typeList,]
+  m <- leaflet() %>%
+    addTiles() %>%
+    # setView(lng = 2.292551, lat = 48.858255, zoom = 16) %>%
+    # addMarkers(lng = data$Longitude, lat = data$Latitude , color="red")
+    addCircles(lng = data$Longitude, lat = data$Latitude , fill = TRUE,color="red" ,opacity = 0.3, weight=12,fillColor = "red",
+               fillOpacity = 0.2)
+  
+  return(m)
+}
+
+
+
+
+userName <-unique(logs$User)
+surveyName <- survey$Name
+
+
+
+UserAndSurvey<-surveyName[surveyName %in% userName]
+
+dataSurveyUser <-survey[survey$Name %in% UserAndSurvey,]
+
+
+
+#table recap
+#Proportion Homme femme
+#proposer un recap par user
+#
+
+
+names(dataSurveyUser)
+
+
+AgeAverage <- function(){
+  AgeAvg <-round(mean(dataSurveyUser$Age))
+  return(AgeAvg)
+}
+
+getAge <- function(user){
+  data<-dataSurveyUser[dataSurveyUser$Name==user,]
+  if (nrow(data) == 0) {Age <- "Unknown"}
+  else{Age <-data$Age}
+  
+  return(Age)
+}
+
+getGender <- function(user){
+  data<-dataSurveyUser[dataSurveyUser$Name==user,]
+  if (nrow(data) == 0) {
+    gender<-"Unknown"
+  } else {
+    gender <-data$Gender
+  }
+  return(gender)
+}
+
+getFamily <- function(user){
+  data<-dataSurveyUser[dataSurveyUser$Name==user,]
+  if (nrow(data) == 0) {
+    family<-"Unknown"
+  } else {
+    family <-data$`Family status`
+  }
+  return(family)
+}
+
+
+getStarted <- function(user){
+  data<-dataSurveyUser[dataSurveyUser$Name==user,]
+  if (nrow(data) == 0) {
+    started<-"Unknown"
+  } else {
+    started <-data$Started
+  }
+  return(started)
+}
+
 # Trend
 d.getTrend <- function(user, typeList) {
   
@@ -189,7 +343,7 @@ d.getTrend <- function(user, typeList) {
   data <- h_toUnix(data)
   
   data <- rename(count(data, data$Type, data$Time), Type = "data$Type", Time = "data$Time")
-
+  
   g <- ggplot(data, aes(Time, n, color=Type))
   
   bar <- g + geom_line(size=1) +
@@ -197,6 +351,8 @@ d.getTrend <- function(user, typeList) {
   
   return (bar)
 }
+
+
 
 # Reason per status
 d.getReasonPerStatus <- function() {
@@ -210,106 +366,40 @@ d.getReasonPerStatus <- function() {
   dataSingle <- data.frame(dataSingle)
   dataSingle["Status"] <- "Single"
   dataSingle <- setNames(cbind(rownames(dataSingle), dataSingle, row.names = NULL), 
-                    c("Reason", "Count", "Status"))
+                         c("Reason", "Count", "Status"))
   
   dataMarried <- colSums(!is.na(dataMarried))
   dataMarried <- data.frame(dataMarried)
   dataMarried["Status"] <- "Married"
   dataMarried <- setNames(cbind(rownames(dataMarried), dataMarried, row.names = NULL), 
-                     c("Reason", "Count", "Status"))
+                          c("Reason", "Count", "Status"))
   
   data <- rbind(dataSingle, dataMarried)
-
+  
   plot <- ggplot(data, aes(x = Reason, y=Count, fill = Status)) +
-            geom_bar(data=subset(data, Status=="Single"), stat="identity") + 
-            geom_bar(data=subset(data, Status=="Married"), stat="identity", aes(y=Count*(-1))) + 
-            scale_y_continuous(
-              breaks = seq(-50, 50, 5), 
-              labels = as.character(c(seq(50, 0, -5), seq(5, 50, 5)))) +
-            coord_flip() +
-            labs(title="Reason to quit per family status") +
-            theme(
-              plot.title = element_text(hjust = .5), 
-              axis.ticks = element_blank()) +
-            scale_fill_brewer(palette = "Dark2")
+    geom_bar(data=subset(data, Status=="Single"), stat="identity") + 
+    geom_bar(data=subset(data, Status=="Married"), stat="identity", aes(y=Count*(-1))) + 
+    scale_y_continuous(
+      breaks = seq(-50, 50, 5), 
+      labels = as.character(c(seq(50, 0, -5), seq(5, 50, 5)))) +
+    coord_flip() +
+    labs(title="Reason to quit per family status") +
+    theme(
+      plot.title = element_text(hjust = .5), 
+      axis.ticks = element_blank()) +
+    scale_fill_brewer(palette = "Dark2")
   
   return (plot)
 }
 
-##### DATA ACCESS  #####
-########################
 
 
 
 
-# workspace fourre tout
 
 
-# data2 <- data
-# data2 <- aggregate(data2$n, by=list(Time = data2$Time), FUN=sum)
-# data2["Type"] = "Total"
-# data2 <- rename(data2, "n" = "x")
-# 
-# da <- rbind(data, data2)
-# da["isTotal"] <- with(da, ifelse(Type == "Total", 0, 1))
-# 
-# data <- surveydataece
-# data <- data[, 51:56]
-# data
-# 
-# data2 <- colSums(!is.na(data))
-# data3 <- data.frame(data2)
-# 
-# data3 <- setNames(cbind(rownames(data3), data3, row.names = NULL), 
-#          c("Reason", "Value"))
-# 
-# data3
-# 
-# # , aes(x="", y=Count, fill=Type)
-# bp <- ggplot(data3, aes(x="", y=Value, fill=Reason)) + geom_bar(width= 1, stat="identity")
-# pie <- bp + coord_polar("y", start=0)
-# pie
-# 
-# ##
-# data4 <- surveydataece[, 9]
-# data5 <- cbind(data, data4)
-# data6 <- colSums(!is.na(data))
-# data6
-# 
-# data7 <- data5[data5$`Family status` == "Single", 1:6]
-# 
-# data10 <- data5[data5$`Family status` == "Married", 1:6]
-# 
-# data8 <- colSums(!is.na(data7))
-# data9 <- data.frame(data8)
-# data9["Status"] <- "Single"
-# data9 <- setNames(cbind(rownames(data9), data9, row.names = NULL), 
-#                    c("Reason", "Count", "Status"))
-# 
-# 
-# data11 <- colSums(!is.na(data10))
-# data12 <- data.frame(data11)
-# data12["Status"] <- "Married"
-# data12 <- setNames(cbind(rownames(data12), data12, row.names = NULL), 
-#                   c("Reason", "Count", "Status"))
-# 
-# data13 <- rbind(data9, data12)
-# 
-# data14 <- setNames(cbind(rownames(data13), data13, row.names = NULL), 
-#                   c("Reason", "Count", "Status"))
-# 
-# # bp <- ggplot(data3, aes(x="", y=Value, fill=Reason)) + geom_bar(width= 1, stat="identity")
-# # pie <- bp + coord_polar("y", start=0)
-# # pie
-# data13
-# 
-# ggplot(data13, aes(x = Reason, y=Count, fill = Status)) +
-#   geom_bar(data=subset(data13, Status=="Single"), stat="identity") + 
-#   geom_bar(data=subset(data13, Status=="Married"), stat="identity", aes(y=Count*(-1))) + 
-#   scale_y_continuous(breaks = seq(-50, 50, 5), 
-#                      labels = as.character(c(seq(50, 0, -5), seq(5, 50, 5)))) +
-#   coord_flip() +
-#   labs(title="Reason to quit per family status") +
-#   theme(plot.title = element_text(hjust = .5), 
-#         axis.ticks = element_blank()) +
-#   scale_fill_brewer(palette = "Dark2")
+
+
+
+
+
