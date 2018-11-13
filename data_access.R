1aca516127a72008118e5bd2b069a495830f8ee8
Alex-Cas <31858792+Alex-Cas@users.noreply.github.com>
2018-11-13 23:00:15 +0100

    percentage

--- data_access.R
+++ data_access.R
@@ -305,19 +305,21 @@ d.getReasonPerStatus <- function() {
   dataSingle <- data.frame(dataSingle)
   dataSingle["Status"] <- "Single"
   dataSingle <- setNames(cbind(rownames(dataSingle), dataSingle, row.names = NULL), 
-                         c("Reason", "Count", "Status"))
+                         c("Reason", "Percentage", "Status"))
+  dataSingle$Percentage <- dataSingle$Percentage / sum(dataSingle$Percentage) * 100
   
   dataMarried <- colSums(!is.na(dataMarried))
   dataMarried <- data.frame(dataMarried)
   dataMarried["Status"] <- "Married"
   dataMarried <- setNames(cbind(rownames(dataMarried), dataMarried, row.names = NULL), 
-                          c("Reason", "Count", "Status"))
+                          c("Reason", "Percentage", "Status"))
+  dataMarried$Percentage <- dataMarried$Percentage / sum(dataMarried$Percentage) * 100
   
   data <- rbind(dataSingle, dataMarried)
   
-  plot <- ggplot(data, aes(x = Reason, y=Count, fill = Status)) +
+  plot <- ggplot(data, aes(x = Reason, y=Percentage, fill = Status)) +
     geom_bar(data=subset(data, Status=="Single"), stat="identity") + 
-    geom_bar(data=subset(data, Status=="Married"), stat="identity", aes(y=Count*(-1))) + 
+    geom_bar(data=subset(data, Status=="Married"), stat="identity", aes(y=Percentage*(-1))) + 
     scale_y_continuous(
       breaks = seq(-50, 50, 5), 
       labels = as.character(c(seq(50, 0, -5), seq(5, 50, 5)))) +
